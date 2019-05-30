70bb645b12d5aa81d4d682e792cf162753fbb832
David Bolin <david@bolin.at>
2019-05-30 13:04:48 -0700

    preparing for deployment

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,9 +1,13 @@
-const { NODE_ENV } = require('../config')
+'use strict';
 
+const { NODE_ENV } = require('../config');
+
+// eslint-disable-next-line no-unused-vars
 module.exports = function errorHandler(error, req, res, next) {
   const response = (NODE_ENV === 'production')
     ? { error: 'Server error' }
-    : (console.error(error), { error: error.message, details: error })
+    // eslint-disable-next-line no-console
+    : (console.error(error), { error: error.message, details: error });
 
-  res.status(500).json(response)
-}
+  res.status(500).json(response);
+};
